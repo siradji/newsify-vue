0a5e0f786ff3c6cdd883059b045c4fecbf7be3f2
Siradji <siradjiawoual@gmail.com>
2021-02-12 11:24:29 +0000

    Init - fresh

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="header">
+        <h1 class="site-title"> Newsify </h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
